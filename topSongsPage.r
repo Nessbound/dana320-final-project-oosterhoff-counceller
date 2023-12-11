6727ad3ffa96d953ab6843b32769664f3059350f
Connor Counceller <111522793+connor-counceller@users.noreply.github.com>
2023-12-11 14:14:23 -0500

    Add selectors and basic graph
    Add category selector
    Add genre selector
    Add basic graph to display top songs

--- topSongsPage.r
+++ topSongsPage.r
@@ -11,6 +11,7 @@ library(ggplot2)
 
 # Load in the music data
 musicData <- get(load("Data/data-music.RData"))
+print(musicData)
 
 # Set up shiny ui
 ui <- fluidPage(
@@ -19,22 +20,50 @@ ui <- fluidPage(
     h1(
       "Top Songs App",
       align = "center"
-      )
-    ),
+    )
+  ),
   
   # Layout
-  layout(
+  sidebarLayout(
     
     sidebarPanel(
       
-      #TODO: Add attribute selection - The attribute of focus, i.e. energy, danceability, most plays, etc.
+      # Attribute of Focus / Category
+      h3(
+        "Category",
+        align = "center"
+      ),
+      
+      # Select category
+      selectizeInput(
+        "attributeOfFocus",
+        "Attribute",
+        choices = NULL
+      ),
+      
+      # Filter Options
+      h3(
+        "More Filter Options",
+        align = "center"
+      ),
+      
+      # Select genre
+      selectizeInput(
+        "genre",
+        "Genre",
+        choices = NULL
+      )
+      
       #TODO: Maybe add a way for multiple categories to be selected? Songs can then be displayed in bar clusters.
       #      Each song has a cluster of 1-3 bars, and each bar is the rating for the selected attributes. - Would need a way to determine top order if multiple attributes are selected
-      #TODO: Add filter options - Genres, artists, etc.
+      #TODO: Add filter options - artists, etc.
       
     ),
     
+    # Main section for displaying list and plot
     mainPanel(
+      
+      plotOutput("topSongsPlot")
       
       #TODO: Display list of top x number of songs from filtered selection, sorted desc by category
       #TODO: Display song titles and artists in big/bold letters, then less important info in smaller sub-text underneath song title/artist. - i.e. album, genre, year, etc.
@@ -50,8 +79,80 @@ ui <- fluidPage(
 
 # Set up shiny server
 server <- function(input, output, session) {
-
+  
+  # Create drop down for attribute of focus
+  updateSelectizeInput(
+    session, 
+    'attributeOfFocus', 
+    # TODO: Use better way - I know there is a way, but can't remember how
+    choices = c(
+      "danceability",
+      "energy",
+      "speechiness",
+      "acousticness",
+      "instrumentalness",
+      "liveness",
+      "popularity"
+    ),
+    server = TRUE
+  )
+  
+  observeEvent(req(input$attributeOfFocus), {
+    updateSelectizeInput(
+      session, 
+      'genre', 
+      choices = c( '[ALL]', 
+                   sort(unique(
+                     musicData$genre
+                   ))
+                ),
+      server = TRUE
+    )
+  })
+  
+  # Create plot
+  output$topSongsPlot <- renderPlot({
+    
+    # Select only needed data
+    filteredMusicData <- musicData |>
+      mutate(song = track_name,
+             artist = artist_name,
+             album = album_name,
+             score = .data[[input$attributeOfFocus]]
+             ) |>
+      select(song, artist, album, genre, score) |>
+      distinct(song, .keep_all = TRUE) |>
+      arrange(desc(score)) |>
+      slice(1:20)
+    
+    if (!(input$genre == '[ALL]')) {
+      filteredMusicData <- filteredMusicData |>
+        filter(genre == input$genre)
+    }
+    
+    #TEST PRINT
+    print(filteredMusicData)
+    
+    
+    # Display graph
+    ggplot(
+      filteredMusicData,
+      aes(
+        x = song,
+        y = score
+      )
+    ) + 
+      geom_col() +
+      labs(
+        title = "Top Songs"
+      )
+  })
 }
 
 # Run shiny app
 shinyApp(ui, server)
+
+# Notes
+# - Top n slider
+# - Ordering of bars on chart
+# - UI: Switch between tabs
