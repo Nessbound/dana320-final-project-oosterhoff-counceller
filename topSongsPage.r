f9d48f7fe243147956a76336174d7a7ac1e423ac
Connor Counceller <111522793+connor-counceller@users.noreply.github.com>
2023-12-13 19:18:42 -0500

    Clean up graph and formatting
    Add ordering to bar graph
    Add color to bars to match the color scheme
    Center graph and list location
    Make filter expandable at the top instead of a pop-up menu

--- topSongsPage.r
+++ topSongsPage.r
@@ -3,6 +3,8 @@ library(tidyverse)
 library(shinyjs)
 library(shiny)
 library(ggplot2)
+library(fmsb)
+library(plotly)
 
 # Load in the music data
 musicData <- get(load("Data/data-music.RData"))
@@ -18,59 +20,66 @@ ui <- fluidPage(
   titlePanel(
     h1(
       "Top Songs App", 
-       align = "center"
-      )
+      align = "center"
+    )
   ),
   
-  # Main section for displaying list and plot
-  mainPanel(
-    
-    # Add a button to toggle the filters popup
-    actionButton("toggleFilters", "Filters", style = "margin-bottom: 20px; margin-left: 20px;"),
-    
-    plotOutput("topSongsPlot"),
-    
-    # Display list of top songs
-    htmlOutput("topSongsList")
-    
-    ),
+  # Display list and plot
+  fluidRow(
+    column(12,
+           # Add a button to toggle the filters popup
+           actionButton("toggleFilters", "Filters", style = "margin-bottom: 20px; margin-left: 20px;")
+    )
+  ),
   
-  # Filters popup
-  shinyjs::hidden(
-    div(
-      id = "filterPopup",
+  fluidRow(
+    # Filters popup
+    shinyjs::hidden(
       div(
-        # Filters
-        h3(
-          "Filters",
-          align = "center"
+        id = "filterPopup",
+        align = "center",
+        div(
+          # Filters
+          h3(
+            "Filters",
+            align = "center"
+          ),
+          
+          # Slider for Top N
+          sliderInput(
+            "topN",
+            "Number of Songs",
+            min = 1,
+            max = 100,
+            value = 10
+          ),
+          
+          # Select category
+          selectizeInput(
+            "attributeOfFocus",
+            "Focus Category",
+            choices = NULL
+          ),
+          
+          # Select genre
+          selectizeInput(
+            "genre",
+            "Genre",
+            choices = NULL
+          ),
         ),
         
-        # Slider for Top N
-        sliderInput(
-          "topN",
-          "Number of Songs",
-          min = 1,
-          max = 100,
-          value = 10
-        ),
-        
-        # Select category
-        selectizeInput(
-          "attributeOfFocus",
-          "Focus Category",
-          choices = NULL
-        ),
-        
-        # Select genre
-        selectizeInput(
-          "genre",
-          "Genre",
-          choices = NULL
-        ),
-      ),
-      
-      style = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 20px; background-color: white; border: 2px solid #ddd; border-radius: 10px;"
+        style = "padding: 20px; background-color: white; border: 2px solid #ddd; border-radius: 10px;"
+      )
+    )
+  ),
+  
+  fluidRow(
+    column(12,
+           plotlyOutput("topSongsPlot"),
+           
+           # Display list of top songs
+           htmlOutput("topSongsList")
     )
   )
 )
@@ -78,51 +87,64 @@ ui <- fluidPage(
 # Set up shiny server
 server <- function(input, output, session) {
   
-  # Create drop down for attribute of focus
-  updateSelectizeInput(
-    session, 
-    'attributeOfFocus',
-    choices = c(
-      "danceability",
-      "energy",
-      "speechiness",
-      "acousticness",
-      "instrumentalness",
-      "liveness",
-      "popularity"
-    ),
-    server = TRUE
+  # Initialize reactive values to store filters
+  rv <- reactiveValues(
+    attributeOfFocus = "Danceability",  # Set default value
+    genre = "[ALL]"  # Set default value
   )
   
-  observeEvent(req(input$attributeOfFocus), {
+  # Create drop-down for attribute of focus
+  observe({
+    updateSelectizeInput(
+      session, 
+      'attributeOfFocus',
+      choices = c(
+        "Danceability",
+        "Energy",
+        "Speechiness",
+        "Acousticness",
+        "Instrumentalness",
+        "Liveness"
+      ),
+      selected = rv$attributeOfFocus
+    )
+  })
+  
+  # Update genre drop-down based on attribute of focus
+  observe({
     updateSelectizeInput(
       session, 
       'genre', 
-      choices = c( '[ALL]', 
-                   sort(unique(musicData$genre))
-      ),
-      server = TRUE
+      choices = c('[ALL]', sort(unique(musicData$genre))),
+      selected = rv$genre
     )
   })
   
   # Create plot and update top songs list
-  output$topSongsPlot <- renderPlot({
+  output$topSongsPlot <- renderPlotly({
     
     # Prepare filtered data
     filteredMusicData <- prepareFilteredData(musicData, input$attributeOfFocus, input$genre, input$topN)
     
-    # Display graph
-    ggplot(
-      filteredMusicData,
-      aes(
-        x = song,
-        y = score
-      )
-    ) + 
-      geom_col() +
-      labs(
-        title = "Top Songs"
-      )
+    # Calculate dynamic y-axis limits - set them slightly below and above min amd max respectively
+    y_min <- min(filteredMusicData$score) * 0.999
+    y_max <- max(filteredMusicData$score) * 1.001
+    
+    # Create a ggplot object
+    base_plot <- ggplot(filteredMusicData, aes(x = reorder(song, -score), y = score, text = paste("Song: ", song, "<br>", input$attributeOfFocus, ": ", score))) +
+      geom_bar(stat = "identity", fill = "#550099") +
+      labs(title = "Top Songs", x = "Song", y = input$attributeOfFocus) +
+      theme_minimal() +
+      theme(axis.text.x = element_blank())  # Remove x-axis text
+    
+    # Convert ggplot object to plotly
+    topSongsPlot <- ggplotly(base_plot, tooltip = c("text"), hoverinfo = "x+y+text")
+    
+    # Set dynamic y-axis limits in layout
+    topSongsPlot <- layout(topSongsPlot, yaxis = list(range = c(y_min, y_max)))
+    
+    # Return the interactive plot
+    topSongsPlot
   })
   
   # Display top songs list
@@ -137,16 +159,16 @@ server <- function(input, output, session) {
       sapply(seq_len(nrow(topSongsList)), function(i) {
         paste(
           "<div style='border: 2px solid #ddd; border-radius: 32px; margin: 16px; display: flex; align-items: center;'>",   # Outer container for song "object"
-          "<div style='font-size: 48px; color: #509; font-weight: bold; margin-left: 16px; margin-right: 12px;'>", i,   # Number text
-          "</div>",
-          "<div style='font-size: 48px; color: #eee; margin-right: 16px;'>", "|",   # Divider line
-          "</div>",
-          "<div style='flex-grow: 1; text-align: left;'>",   # Container for song and artist text
-          "<div style='font-size: 24px; color: #000; font-weight: bold;'>", topSongsList[i, "song"],   # Song text
-          "</div>",
-          "<div style='font-size: 16px; color: #000;'>", topSongsList[i, "artist"],   # Artist text
-          "</div>",
-          "</div>",
+            "<div style='font-size: 48px; color: #509; font-weight: bold; margin-left: 16px; margin-right: 12px;'>", i,   # Number text
+            "</div>",
+            "<div style='font-size: 48px; color: #eee; margin-right: 16px;'>", "|",   # Divider line
+            "</div>",
+            "<div style='flex-grow: 1; text-align: left;'>",   # Container for song and artist text
+              "<div style='font-size: 24px; color: #000; font-weight: bold;'>", topSongsList[i, "song"],   # Song text
+              "</div>",
+              "<div style='font-size: 16px; color: #000;'>", topSongsList[i, "artist"],   # Artist text
+              "</div>",
+            "</div>",
           "</div>",
           sep = ""
         )
@@ -172,7 +194,7 @@ prepareFilteredData <- function(musicData, attributeOfFocus, selectedGenre, topN
     mutate(song = track_name,
            artist = artist_name,
            album = album_name,
-           score = .data[[attributeOfFocus]]
+           score = as.numeric(.data[[tolower(attributeOfFocus)]])
     ) |>
     select(song, artist, album, genre, score) |>
     arrange(desc(score)) |>
@@ -191,9 +213,3 @@ prepareFilteredData <- function(musicData, attributeOfFocus, selectedGenre, topN
 
 # Run shiny app
 shinyApp(ui, server)
-
-
-# TODO:
-# - Make the visual better - remove song names or make them visibly only when you hover over them, decide on best graph type, etc.
-# - Center graph and list onto the screen.
-# - Add ordering to bar chart!
